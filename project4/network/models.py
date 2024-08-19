30d41c99c7c8b5914c8e0c86865d2c557e5a1b61
Saidxusaynbek Bobojonov <saidxusenbobojonov@gmail.com>
2024-08-19 16:51:22 +0500

    Made starting changes

--- project4/network/models.py
+++ project4/network/models.py
@@ -4,3 +4,9 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Post(models.Model):
+    user = models.ForeignKey("User", on_delete=models.CASCADE, related_name="owner")
+    content = models.CharField(max_length=300)
+    timestap = models.DateTimeField(auto_now_add=True)
+
